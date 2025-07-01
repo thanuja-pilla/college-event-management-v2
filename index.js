bec1f5cd7e542d2137c270167aa129ec0b9d8077
Thanuja Pilla <thanujapilla7@gmail.com>
2025-07-01 05:34:23 +0000

    google meet

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config(); //to load env variables
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
